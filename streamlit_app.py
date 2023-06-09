bc4c65ec9f2d72c01f70aaf46cd1574bfdee3851
BLThomson67 <136099090+BLThomson67@users.noreply.github.com>
2023-06-09 08:56:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit
 
-streamlit.title('My parentsnew healthy diner. probably gonna fail. we should sell weed instead.')
+streamlit.title('My parentsnew healthy diner. probably gonna fail. we should sell weed instead. Doper Menu and Pink Floyd Music.')
